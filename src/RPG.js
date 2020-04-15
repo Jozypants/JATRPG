011af62ffb186ae9cae7fde053b62723aeb38229
Jozy Kinnaman <jozypants@gmail.com>
2020-04-15 14:26:56 -0700

    test spec 'character constructor holds separate values for peeWee object

--- src/RPG.js
+++ src/RPG.js
@@ -2,13 +2,44 @@
 
 export class Game {
   constructor(character, currentPlayer, currentAction) {
-    // this.character = peeWee, davidBowie, elVira;
-    this.character = character;
+    this.character = peeWee, davidBowie, elVira;
+    // this.character = character;
     this.currentPlayer = [];
     this.currentAction = [];
     this.level= 0;
   }
-
   // methods for game -->
 
 }
+
+
+export class Character {
+  constructor() {
+    this.peeWee = { weapon: "bike", action: "dance", level: 0 }
+    this.davidBowie = { weapon: "glitter", action: "jump", level: 1 }
+    this.elVira = { weapon: "dagger", action: "spell", level: 0 }
+  }
+}
+
+// let playerOne = new Character ()
+// playerOne.peeWee; 
+
+//     let peeWee =  { 
+//       name: "Pee Wee",
+//       weapon: "acid bowtie",
+//       fightingStyle: "trickster"
+//     }
+//     let davidBowie = {
+//       name: ""
+//     } 
+//   };
+// }
+//   this.elVira = 
+//   this.davidBowie = 0;
+
+  // chooseCharacter(choice) {
+  //   if(choice == peeWee) {
+  //     this.weapon = peeWeeWeapon;
+
+  //   }
+  // }
