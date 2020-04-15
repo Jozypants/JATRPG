76621f24e7c3c105803f9d64fa4c34c70fac32b8
Jozy Kinnaman <jozypants@gmail.com>
2020-04-15 13:26:33 -0700

    test spec 'constructor holds values'

--- __tests__/RPG.tests.js
+++ __tests__/RPG.tests.js
@@ -3,9 +3,9 @@ import { Game } from './../src/RPG.js';
 describe('Game', () => {
   test('game constructor holds inputted values', () => {
     let game = new Game("peeWee", "peeWee", "fighting");
-    expect(game.character.toEqual("peeWee"));
-    expect(game.currentPlayer.toEqual('peeWee'));
-    expect(game.currentAction.toEqual('fighting'));
-    expect(game.level.toEqual(0));
+    expect(game.character).toEqual("peeWee");
+    expect(game.currentPlayer).toEqual(['peeWee']);
+    expect(game.currentAction).toEqual('fighting');
+    expect(game.level).toEqual(0);
   })
 })
