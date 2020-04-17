e6380796dc111f1d62607877d9b951122e5229c3
Jozy Kinnaman <jozypants@gmail.com>
2020-04-16 20:37:53 -0700

    update css and main.js

--- src/RPG.js
+++ src/RPG.js
@@ -2,24 +2,42 @@
 export class Player {
   constructor(currentPlayer) {
     this.player = {
-      peeWee: { weapon: "bike", action: "dance", hp: 30, level: 0 },
-      elVira: { weapon: "dagger", action: "spell", hp: 40,level: 0 },
-      davidBowie: { weapon: "glitter", action: "jump", hp: 50, level: 0 },
-    }
+      peeWee: {
+        name: "Pee Wee",
+        weapon: "bike",
+        action: "dance",
+        hp: 30,
+        level: 0,
+      },
+      elVira: {
+        name: "El Vira",
+        weapon: "dagger",
+        action: "spell",
+        hp: 40,
+        level: 0,
+      },
+      ziggyStardust: {
+        name: "Ziggy Stardust",
+        weapon: "glitter",
+        action: "jump",
+        hp: 50,
+        level: 0,
+      },
+    };
     this.currentPlayer = currentPlayer;
     this.currentRoll = 0;
   }
   // methods for each character  -->
   chooseCharacter(choice) {
     if (choice === " ") {
-      return this.currentPlayer = "Please choose a character";
-    } else if (choice === "peeWee") {
+      return (this.currentPlayer = "Please choose a character");
+    } else if (choice === "Pee Wee") {
       this.currentPlayer = this.player.peeWee;
       // return "peeWee";
-    } else if (choice === "davidBowie") {
-      this.currentPlayer = this.player.davidBowie;
+    } else if (choice === "Ziggy Stardust") {
+      this.currentPlayer = this.player.ziggyStardust;
       // return "davidBowie"
-    } else if (choice === "elVira") {
+    } else if (choice === "El Vira") {
       this.currentPlayer = this.player.elVira;
       // return "elVira"
     }
@@ -29,9 +47,9 @@ export class Player {
   discoverWeapon() {
     this.currentPlayer.hp = this.currentPlayer.hp + 5;
     return this.currentPlayer.weapon;
-  };
+  }
 
-  fightChoice(choice){
+  fightChoice(choice) {
     if (choice === "weapon") {
       this.currentPlayer.hp = this.currentPlayer.hp - 5;
       return this.currentPlayer.weapon;
@@ -39,24 +57,21 @@ export class Player {
       this.currentPlayer.hp = this.currentPlayer.hp - 10;
       return this.currentPlayer.action;
     }
-  };
-  
+  }
+
   rollDice() {
     let random = Math.floor(Math.random() * 6) + 1;
     this.currentRoll += random;
     return this.currentRoll;
-  };
-  
+  }
+
   determineFate() {
     if (this.currentRoll >= 4) {
       this.currentPlayer.hp = this.currentPlayer.hp + 20;
       this.currentPlayer.level = this.currentPlayer.level + 2;
       return true;
     } else if (this.currentRoll <= 3) {
-        return false;
-      }
-  };
-};
-
-
-
+      return false;
+    }
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,10 +1,38 @@
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles.css';
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles.css";
+import "./animate.css";
 import $ from "jquery";
-
+import {Player} from './../src/RPG.js';
 
 //User Interface Logic -->
+$(document).ready(function () {
+  let characters = [
+  { name: "Pee Wee"},
+  { name: "Ziggy Stardust"},
+  { name: "El Vira"},
+  ];
+  characters.forEach(function (character) {
+    $("#characters").append(
+      `<option value="${character.name}">${character.name}</option>`
+    );
+  });
+  $("#players").submit(function(event) {
+    event.preventDefault();
+    // let playerOne = new Player();
+    let userInput = $("#characters").val();
+    let character = playerOne.chooseCharacter(userInput);
+    
+    if (character === "Pee Wee") {
+      $("#pee-wee").slideDown("slow");
+      $("#start-page").hide();
+    } else if (character === "El Vira") {
+      $("#el-vira").slideDown("slow");
+      $("#start-page").hide();
+    } else if (character === "Ziggy Stardust") {
+      $("#ziggy-stardust").slideDown("slow");
+      $("#start-page").hide();
+    } 
+  });
 
-$(document).ready()
-
+});
